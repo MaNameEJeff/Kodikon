bf78f446818a2502672b75de0e1092943a6a24ef
RockerBot <rockerbotmail@gmail.com>
2022-09-11 03:13:08 +0530

    back and forth in app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def reach():
 	return render_template("index.html")
 
 
-@app.route("/login", methods=["GET, POST"])
+@app.route("/login", methods=["GET"])
 def login():
 	return render_template("login.html")
 
 
-@app.route("/register", methods=["GET, POST"])
+@app.route("/register", methods=["GET"])
 def register():
 	return render_template("register.html")
 
