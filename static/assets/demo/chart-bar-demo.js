67b08a3027b34a37fa037f1b3db2571f9e98cc76
VarunKamath <varunkamath05@gmail.com>
2022-09-11 01:32:11 +0530

    Dashboard changes and some tweaks to dashboard

--- static/assets/demo/chart-bar-demo.js
+++ static/assets/demo/chart-bar-demo.js
@@ -12,7 +12,7 @@ var myLineChart = new Chart(ctx, {
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [168, 210, 419, 420, 900],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 1000,
           maxTicksLimit: 5
         },
         gridLines: {
